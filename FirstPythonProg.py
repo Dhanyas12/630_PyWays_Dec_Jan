70b18c409a2230cabdf5e11f3106c03e601253d4
Dhanyas12 <dhanyamadivada@gmail.com>
2017-12-22 08:39:46 -0500

    Day-1

---
+++ FirstPythonProg.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+
+#! : Shebang
+'/usr/bin/python : Absolute path to the python software'
+
+
+#Single Line Comments
+'''
+1. ''
+2. " "
+3. ''' '''
+4. """ """
+5. #
+'''
+
+"Multi Line Comments"
+"""
+1. ''' '''
+2. """ """
+"""
+
+#Creating Variables in Python
+'''
+1.A-Z
+2.a-z
+3._
+4.0-9 Note: we cannot use Numbers as prefinx variable name
+'''
+FirstName='guido'
+_Middle_Name="Van"
+lastName="Rossum"
+SoftwareVersion=2.7
+_year_DETAILS=1980
+_001 =10
+
+#Calling Variables in Python
+
+print(FirstName)
+print("")
+print(_Middle_Name)
+print("")
+print(lastName)
+print("")
+print(SoftwareVersion)
+print("")
+print(_year_DETAILS)
+print("")
+print(_001)
+print("")
+
+
+
+
+
+
