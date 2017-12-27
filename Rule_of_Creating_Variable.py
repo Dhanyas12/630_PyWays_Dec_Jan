45070019bc08521ae567ff0ca2801e3aa7e9d372
Dhanyas12 <34728662+Dhanyas12@users.noreply.github.com>
2017-12-27 11:54:14 -0500

    Add files via upload

---
+++ Rule_of_Creating_Variable.py
@@ -0,0 +1,26 @@
+#Rule of Creating a Variables in python
+''' Multiline comment with the help of single triples
+1. A-Z        : Uppercase Alphabets
+2. a-z        : Lowercase Alphabets
+3. _          : Special chaacter i.e. Underscore _
+4. 0-9        : Digits
+5. A-Za-z     : Combination of uppercase and lowercase alphabets i.e A-Za-z
+6. A-Za-z_    : Combination of uppercase, Lowercase and special character i.e. _
+7. A-Za-z_0-9 : Conbination of uppercase, lowercase, special characteri.e _ and Digits i.e. 0-9
+Note: A variable cannot be created with prefix as digit i.e. 0-9
+'''
+
+print("Uppercase Alphabets i.e. A-Z")
+print("")
+print("Lowercase Alphabets i.e. a-z")
+print("")
+print("Special Character i.e. Underscore _")
+print("")
+print("Digits i.e. 0-9")
+print("")
+print("Combination of uppercase and lowercase Alphabets i.e. A-Za-z")
+print("")
+print("Combination of uppercase,lowercase Alphabets,special character i.e. _")
+print("")
+print("Combination of upprcase,lowercase Alphabets, special character i.e. _ and Digits i.e. 0-9")
+print("Combination of upprcase,lowercase Alphabets, special character i.e. _ and Digits i.e. 0-9")
